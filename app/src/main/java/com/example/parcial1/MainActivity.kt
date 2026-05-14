6aa69d588883a082d6a59ef4750e256ec09aff71
navasmend0126 <omar.navas@utp.ac.pa>
2026-05-14 17:35:59 -0500

    Aplicacion de logica

--- app/src/main/java/com/example/parcial1/MainActivity.kt
+++ app/src/main/java/com/example/parcial1/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.parcial1
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +13,33 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        //enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val etAmount = findViewById<EditText>(R.id.etAmount)
+        val spFrom = findViewById<Spinner>(R.id.spFrom)
+        val spTo = findViewById<Spinner>(R.id.spTo)
+        val tvResult = findViewById<TextView>(R.id.tvResult)
+        val btnConvert = findViewById<Button>(R.id.btnConvert)
+
+        btnConvert.setOnClickListener {
+            val cantidad = etAmount.text.toString().toDouble()
+            val monedaOrigen = spFrom.selectedItem.toString()
+                .substringAfterLast("(").substringBefore(")")
+
+            val monedaDestino = spTo.selectedItem.toString()
+                .substringAfterLast("(").substringBefore(")")
+
+            val conversor = Conversor()
+            try {
+                val resultado = conversor.convertir(monedaOrigen, monedaDestino, cantidad)
+                tvResult.text = resultado.toString()
+            } catch (e: IllegalArgumentException) {
+                tvResult.text = e.message
+
+            }
         }
+
+
     }
 }
